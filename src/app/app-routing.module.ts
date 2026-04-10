a7acde410af7413549c07becca7ac352de1e1dc6
Siria <sirifranco00@gmail.com>
2026-04-09 20:05:36 -0700

    router module basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
+  {
+    path: 'portfolio',
+    loadChildren: () =>
+      import('./features/portfolio/portfolio.module').then(m => m.PortfolioModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/features/portfolio/pages/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/pages/portfolio/portfolio.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
-export class PortfolioComponent {
 
+export class PortfolioComponent implements OnInit {
+
+  viewType: 'architecture' | 'software' | 'all' = 'all';
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const type = params.get('type');
+
+      if (type === 'architecture') {
+        this.viewType = 'architecture';
+      } else if (type === 'software') {
+        this.viewType = 'software';
+      } else {
+        this.viewType = 'all';
+      }
+
+      console.log('Current view:', this.viewType);
+    });
+  }
 }
