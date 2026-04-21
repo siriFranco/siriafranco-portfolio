4319b341034e20d207f84f622981f51083c8bf2f
Siria <sirifranco00@gmail.com>
2026-04-21 12:10:53 -0700

    tooltip global

--- src/app/features/resume/config/certifications.config.ts
+++ src/app/features/resume/config/certifications.config.ts
@@ -7,11 +7,29 @@ export interface Certification {
   year: string;
   duration?: string;
   description?: string;
+  skills?: string[];
   link?: string;
   type: CertificationType; // 👈 clave para agrupar
 }
 
 export const CERTIFICATIONS: Certification[] = [
+    {
+    title: 'Java Full Stack Developer Program',
+    issuer: 'Generation México',
+    year: '2023',
+    duration: '480h',
+    type: 'tech',
+    description: 'Full stack training program focused on Java development, backend and frontend integration, and professional skills for software development.',
+    skills: [
+      'HTML',
+      'CSS',
+      'JavaScript',
+      'Java',
+      'Spring Boot',
+      'Soft Skills'
+    ],
+    link: 'https://drive.google.com/file/d/1iHhCBdJ1twx-ba9wA9vq-kd0F8c2RTRp/view',
+  },
   {
     title: 'Angular - The Complete Guide',
     issuer: 'Udemy',
